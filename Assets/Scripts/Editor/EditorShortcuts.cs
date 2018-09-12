2083256003557c952cf2308f89144e4ca769adc0
Pavel Neyman <tushqan1@mail.ru>
2018-09-12 19:05:17 +0700

    Добавлено применение характеристик кораблей из ScriptableObject'ов.

---
+++ Assets/Scripts/Editor/EditorShortcuts.cs
@@ -0,0 +1,17 @@
+﻿namespace Editor
+{
+    using UnityEditor;
+
+    public static class EditorShortcuts
+    {
+        /// <summary>
+        /// Блокировка инспектора.
+        /// </summary>
+        [MenuItem("Tools/Toggle Inspector Lock %t")] // Ctrl + T
+        private static void ToggleInspectorLock()
+        {
+            ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+            ActiveEditorTracker.sharedTracker.ForceRebuild();
+        }
+    }
+}
--- Assets/Scripts/Entities/Unit.cs
+++ Assets/Scripts/Entities/Unit.cs
@@ -1,5 +1,7 @@
 ﻿namespace Entities
 {
+    using System.Linq;
+
     using Data;
 
     using UnityEngine;
@@ -7,18 +9,57 @@
     public class Unit : MonoBehaviour
     {
         /// <summary>
+        /// Текущиее здоровье юнита.
+        /// </summary>
+        [SerializeField]
+        private float _currentHealth;
+
+        /// <summary>
+        /// Подвижность.
+        /// </summary>
+        [SerializeField]
+        private float _currentMobility;
+
+        /// <summary>
+        /// Скорость передвижения.
+        /// </summary>
+        [SerializeField]
+        private float _currentMovingSpeed;
+
+        /// <summary>
+        /// Скорострельность.
+        /// </summary>
+        [SerializeField]
+        private float _currentShootingSpeed;
+
+        /// <summary>
         /// Строковый идетификатор юнита.
         /// </summary>
         public string Sid;
-        
+
         /// <summary>
-        /// Базовые характеристика юнита.
+        /// Инициализация характеристик коробля.
         /// </summary>
-        public BaseShipStats BaseShipStats;
-        
-        /// <summary>
-        /// Модификаторы характеристик юнита.
-        /// </summary>
-        public ShipStatsMultipliers ShipStatsMultipliers;
+        private void InitializeAttributes()
+        {
+            var baseShipStats = ShipStats.Instance.BaseShipStats.FirstOrDefault(stats => Sid.Contains(stats.Type));
+            var shipStatsMultipliers = ShipStats.Instance.ShipStatsMultipliers.FirstOrDefault(stats => stats.Type == Sid);
+
+            if (baseShipStats == null || shipStatsMultipliers == null)
+            {
+                Debug.Log($"Не удалось найти характеристики: {Sid}");
+                return;
+            }
+
+            _currentHealth = baseShipStats.Health * shipStatsMultipliers.Health;
+            _currentShootingSpeed = baseShipStats.ShootSpeed * shipStatsMultipliers.ShootSpeed;
+            _currentMovingSpeed = baseShipStats.MoveSpeed * shipStatsMultipliers.MoveSpeed;
+            _currentMobility = baseShipStats.Mobility * shipStatsMultipliers.Mobility;
+        }
+
+        private void Start()
+        {
+            InitializeAttributes();
+        }
     }
 }
